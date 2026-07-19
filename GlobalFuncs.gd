cc3b99bdbeb4e232197c4c19d54dd2519c38d002
epicdeveloper <veryepicgames2@gmail.com>
2026-07-19 18:08:57 -0400

    maybe make it work???

--- GlobalFuncs.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-func wait(time: float):
-	await get_tree().create_timer(time).timeout
--- reset_data.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-signal reset_data
-func _on_pressed() -> void:
-	SaveLoad.Data.points = ENum.new(0,0)
-	SaveLoad.Data.coolerpoints = ENum.new(0,0)
-	reset_data.emit()
